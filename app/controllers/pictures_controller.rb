237ad70ce89792ac516e479ad0e127b4b7619fe7
Phathdt <phathdt.leviateam@gmail.com>
2017-07-18 16:13:19 +0700

    add link_to, fix some bug, routing

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,26 +1,26 @@
 class PicturesController < ApplicationController
   before_action :set_picture, only: [:show, :edit, :update, :destroy]
-  	def update
-  		if @picture.update(picture_params)
-  			redirect_to @picture
-  		else
-  			render 'edit'
-  		end
-  	end
+  def update
+    if @picture.update(picture_params)
+      redirect_to @picture
+    else
+      render 'edit'
+    end
+  end
 
-  	def destroy
-  		@album = @picture.album
-  		@picture.destroy
-  		redirect_to @album 
-  	end
+  def destroy
+    @picture.destroy
+    redirect_to @album
+  end
 
-	private
+  private
 
-	def set_picture
-		@picture = Picture.find(params[:id])
-	end
+  def set_picture
+    @picture = Picture.find(params[:id])
+    @album = @picture.album
+  end
 
-	def picture_params
-		params.require(:picture).permit(:title, :description)
-	end
+  def picture_params
+    params.require(:picture).permit(:title, :description)
+  end
 end
