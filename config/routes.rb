ed48af3238fc19a12db8f2292ba18c60207be219
Phathdt <Phathdt.leviateam@gmail.com>
2017-07-18 20:59:53 +0700

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
-  root 'albums#index'
+  root 'users#index'
   devise_for :users, controllers: {
     sessions: 'users/sessions'
+    # resources :albums
+
   }
+  resources :users do
+    resources :albums
+    resources :pictures
+  end
   # resources :albums do
   #   resources :pictures
   # end
-  resources :albums
-  resources :pictures
-  get '/users' => "users#index"
-  get '/users/:id' => "users#show", as: :user
+  # resources :albums
+  # resources :pictures
+  # get '/users' => "users#index"
+  # get '/users/:id' => "users#show", as: :user
 end
