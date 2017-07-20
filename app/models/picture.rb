178a1c8f1c06f2415c92f5916539ed68a2616f03
Phathdt <phathdt.leviateam@gmail.com>
2017-07-20 11:09:55 +0700

    add callback change quality to album in picture model

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,7 +3,20 @@ class Picture < ApplicationRecord
 
   has_many :likes, as: :likeable
   has_many :liked_users, through: :likes,source: :user
- 
+
+  after_create :increase_album_quality
+  after_destroy :decrease_ablum_quality
+
   has_attached_file :image
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  private
+
+  def increase_album_quality
+    album.increment!(:quality)
+  end
+
+  def decrease_ablum_quality
+    album.decrement!(:quality)
+  end
 end
