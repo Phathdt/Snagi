5b6ca044de8cd26f82d873d2ee61cac412d34da7
Phathdt <phathdt.leviateam@gmail.com>
2017-07-20 15:29:07 +0700

    add name to User and change Parameter User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,20 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   before_action :authenticate_user!, except:[:index, :show]
   before_action :load_notification
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:name, :email,:password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+      user_params.permit(:name, :email,:password, :password_confirmation, :current_password)
+    end
+  end
+
+
   def load_notification
     if user_signed_in?
       @notifications = NotificationService.new({user_id:current_user.id}).show_notifications
