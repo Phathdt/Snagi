5ef7f5fd3073ef501174b0bc7ce6c6a233a783a9
Phathdt <phathdt.leviateam@gmail.com>
2017-07-21 14:24:49 +0700

    Change UI devise

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( devise.css bootstrap.min.css material-kit.css jquery.min.js material.min.js material-kit.js bootstrap.min.js )
