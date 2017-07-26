f56d4279efb095dc8e39d57a9973412a9b0e2358
Phathdt <phathdt.leviateam@gmail.com>
2017-07-26 09:23:45 +0700

    fix logic notification

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,5 +2,5 @@ class Notification < ApplicationRecord
   belongs_to :user
   scope :count_un_readed, ->{ where(is_readed:false).count}
 
-  validates_uniqueness_of :user_id, scope: [:user_has_id, :object_id]
+  validates_uniqueness_of :user_id, scope: [:user_has_id, :object_id, :is_readed]
 end
