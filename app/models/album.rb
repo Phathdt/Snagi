fb45ef135c7306c463f45de402682b8eb5b0e3e4
Phathdt <phathdt.leviateam@gmail.com>
2017-07-26 10:10:21 +0700

    Fix bug notification

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,8 +1,10 @@
 class Album < ApplicationRecord
   scope :belongs_to_user, ->(user_id){where('user_id = ?', user_id)}
   scope :not_private, ->{ where(is_private:false) }
+  
 
-  after_update :send_notification
+  after_create :send_notification_user
+  after_update :send_notification_album
   belongs_to :user
   has_many :pictures, dependent: :destroy
 
@@ -13,7 +15,12 @@ class Album < ApplicationRecord
   has_many :followed_users, through: :follows,source: :user
 
   private 
-  def send_notification
+  def send_notification_user
+    user = self.user
+    NotificationService.new({type:"User", id:user.id, album_id:id}).send_notifications 
+  end
+
+  def send_notification_album
   	NotificationService.new({type:"Album",id: id}).send_notifications	
   end
 end
--- app/services/notification_service.rb
+++ app/services/notification_service.rb
@@ -3,20 +3,32 @@ class NotificationService
     @type = params[:type]
     @id = params[:id]
     @album = Album.find_by(id:@id)
+    @album_id = params[:album_id]
   end
 
   def send_notifications
-    user_ids = Follow.where("followable_id = ? AND followable_type = ? ", @id, @type).pluck(:user_id)
+    user_ids = Follow.where("followable_id = ? AND followable_type = ? ",
+                            @id, @type).pluck(:user_id)
     Notification.transaction do
-    	user_ids.each do |user_id|
-        album = Album.find(@id)
-    		Notification.create(user_id:user_id, content:"Album #{@id} change", user_has_id: album.user.id, object_id: @id)
-    	end
+      if @type == "Album"
+        user_ids.each do |user_id|
+          album = Album.find(@id)
+          Notification.create(user_id:user_id, content:"Album #{@id} change",
+                              user_has_id: album.user.id, object_id: @id)
+        end
+      else
+        user_ids.each do |user_id|
+          album = Album.find(@album_id)
+          Notification.create(user_id:user_id, content:"Album #{@album_id} create",
+                              user_has_id: album.user.id, object_id: @id)
+        end
+      end
+
     end
   end
 
   def self.count_un_read(user_id)
-  	Notification.where(user_id:user_id).count_un_readed
+    Notification.where(user_id:user_id).count_un_readed
   end
 
   def self.show_notifications(user_id)
