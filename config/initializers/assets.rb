c5ad52200c33ee7ed070bd942d99adec45cd3967
Phathdt <Phathdt.leviateam@gmail.com>
2017-07-26 22:36:57 +0700

    remove text

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( devise.css)
 Rails.application.config.assets.paths += Dir["#{Rails.root}/vendor/*"].sort_by { |dir| -dir.size }
